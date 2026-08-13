9217067f9c207a3832c33c2ee3e44fb3f43bf011
ARJUN-BAM <arjunbam2003@gmail.com>
2026-08-13 07:12:51 +0530

    Backend skeleton: Spring Boot + H2

---
+++ src/main/java/com/algoworkspace/algoworkspace_backend/AlgoworkspaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.algoworkspace.algoworkspace_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoworkspaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoworkspaceBackendApplication.class, args);
+	}
+
+}
